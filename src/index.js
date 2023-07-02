6e9040651bfc0d1be45ead823f0b90c76d97f0ac
Kolko Valentin <valiant9191@gmail.com>
2023-07-03 01:29:07 +0400

    feat: basic http;

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('hello')
+import http from 'node:http';
+
+
+const PORT = 4000;
+
+const server = http.createServer({}, (req, res) => {
+    switch (req.url) {
+        case '/api/users':
+            res.writeHead(200);
+            res.end('hello world\n');
+            break;
+
+        default:
+            res.writeHead(404);
+            res.end('not found\n')
+            break;
+    }
+})
+
+server.listen(PORT || 4000, (err) => {
+    if (err) {
+        console.error('Something bad happened' + err.message);
+    } else {
+        console.log(`server is listening on ${PORT}`);
+    }
+})
